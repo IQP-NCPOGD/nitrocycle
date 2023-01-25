dd10c92a4491d6f965d54f58ad4e8087c357e23f
Ethan Pollack <epollack31@gmail.com>
2023-01-24 19:57:28 -0500

    save

--- src/index.js
+++ src/index.js
@@ -4,11 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ARCanvas, ARMarker } from "@artcom/react-three-arjs"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(<>
+	<ARCanvas	camera={ { position: [0, 0, 0] } }
+    					onCreated={ ({ gl }) => {
+      					gl.setSize(window.innerWidth, window.innerHeight)
+    					} } >
+    	<ambientLight />
+    	<pointLight position={ [10, 10, 0] }  />
+			<ARMarker	type={ "pattern" } patternUrl={ "data/hiro.patt" }>
+      	<mesh>
+        	<boxBufferGeometry args={ [1, 1, 1] } />
+        	<meshStandardMaterial color={ "green" } />
+      	</mesh>
+    	</ARMarker>
+		</ARCanvas>
+
   <React.StrictMode>
-    <App />
+  	<App />
   </React.StrictMode>
+	</>
 );
 
 // If you want to start measuring performance in your app, pass a function
