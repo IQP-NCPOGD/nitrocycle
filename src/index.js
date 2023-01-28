9c24ca60dfb80ff45be6ef933b6f191a0b230def
Ethan Pollack <epollack31@gmail.com>
2023-01-28 00:02:55 -0500

    working with arjs!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = () => <h1>test, React!</h1>;
+import { ARCanvas, ARMarker } from "@artcom/react-three-arjs";
 
-render(<App />, document.getElementById('root'));
-
+render(
+  <ARCanvas
+    camera={ { position: [0, 0, 0] } }
+    onCreated={ ({ gl }) => {
+      gl.setSize(window.innerWidth, window.innerHeight)
+    } }>
+    <ambientLight />
+    <pointLight position={ [10, 10, 0] }  />
+    <ARMarker
+      type={ "pattern" }
+      patternUrl={ "data/hiro.patt" }>
+      <mesh>
+        <boxBufferGeometry args={ [1, 1, 1] } />
+        <meshStandardMaterial color={ "green" } />
+      </mesh>
+    </ARMarker>
+  </ARCanvas>,
+  document.getElementById("root")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 const path = require( 'path' );
-
 module.exports = {
    context: __dirname,
    entry: './src/index.js',
@@ -31,6 +31,12 @@ module.exports = {
       new HtmlWebPackPlugin({
          template: path.resolve( __dirname, 'public/index.html' ),
          filename: 'index.html'
-      })
+      }),
+		 new CopyWebpackPlugin({
+      patterns: [
+        { from: "./node_modules/@ar-js-org/artoolkit5-js/data/", to: "data/" },
+        { from: "./data/", to: "data/" },
+      ],
+    }),
    ]
 };
