0197279e61b727819908332d7c1c0f270c89b218
Ethan Pollack <epollack31@gmail.com>
2023-01-27 22:14:35 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { useState } from 'react';
+import { ZapparCamera, InstantTracker, ZapparCanvas } from '@zappar/zappar-react-three-fiber';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function App() {    
+	let [placementMode, setPlacementMode] = useState(true);
   return (
+		<>
+		<ZapparCanvas>
+          <ZapparCamera />
+          <InstantTracker placementMode={placementMode} placementCameraOffset={[0, 0, -5]}>
+            <mesh>
+              <sphereBufferGeometry />
+              <meshStandardMaterial color="hotpink" />
+            </mesh>
+          </InstantTracker>
+          <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
+
+        </ZapparCanvas>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +36,7 @@ function App() {
         </a>
       </header>
     </div>
+		</>
   );
 }
 
