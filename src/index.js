017008f46e341481c07b151aaf349724b0331264
Ethan Pollack <epollack31@gmail.com>
2023-02-03 12:51:08 -0500

    save

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { ARCanvas, ARMarker } from "@artcom/react-three-arjs";
 
-render(
-  <ARCanvas
-    camera={ { position: [0, 0, 0] } }
-    onCreated={ ({ gl }) => {
-      gl.setSize(window.innerWidth, window.innerHeight)
-    } }>
-    <ambientLight />
-    <pointLight position={ [10, 10, 0] }  />
-    <ARMarker
-      type={ "pattern" }
-      patternUrl={ "data/hiro.patt" }>
-      <mesh>
-        <boxBufferGeometry args={ [1, 1, 1] } />
-        <meshStandardMaterial color={ "green" } />
-      </mesh>
-    </ARMarker>
-  </ARCanvas>,
+import './main.css';
+const container = document.getElementById('app');
+const root = createRoot(container); 
+
+root.render(
+	<React.StrictMode>
+	<>
+		<div className='ui'>
+			<h1 className='title'>NitroCycle</h1>
+		</div>
+  	<ARCanvas camera={ { position: [0, 0, 0] } }
+  	  onCreated={ ({ gl }) => {
+   	   gl.setSize(window.innerWidth, window.innerHeight)
+   	 } }>
+   		<ambientLight />
+			<pointLight position={ [10, 10, 0] }  />
+			<ARMarker type={ "pattern" } 
+								patternUrl={ "data/hiro.patt" }>
+   	  	<mesh>
+   	    	<boxBufferGeometry args={ [1, 1, 1] } />
+   	    	<meshStandardMaterial color={ "green" } />
+   	  	</mesh>
+			</ARMarker>
+		</ARCanvas>
+	</>
+	</React.StrictMode>,
   document.getElementById("root")
 );
