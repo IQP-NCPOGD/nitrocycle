b34c07621a1b8940de06a31f83f70ab120dac352
phbui <101759075+phbui@users.noreply.github.com>
2023-02-12 21:02:08 -0500

    Fixed Spelling Error

--- src/components/Menu/FarmersLog/Pages/Resources/NitrogenRunoff.js
+++ src/components/Menu/FarmersLog/Pages/Resources/NitrogenRunoff.js
@@ -11,7 +11,7 @@ function NitrogenRunoff(props) {
                 <p>This is the page for the Nitrogen Runoff.</p>
                 <div class='true-center'>
                     <button class="pressable" type="button" onClick={props.update("Home")}>
-                        <div class='item-bame'>
+                        <div class='item-name'>
                             <h4>Home</h4>
                         </div> 
                         <img src="../../../../../data/images/potato.png" alt="buttonpng" border="0"/>
