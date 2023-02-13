2971a5439cdd17a4cc34456bf2549f4b1ace1e81
phbui <101759075+phbui@users.noreply.github.com>
2023-02-12 20:39:06 -0500

    Farmer's Log TOC UI Update

--- src/components/Menu/FarmersLog/Pages/FarmersLogHome.js
+++ src/components/Menu/FarmersLog/Pages/FarmersLogHome.js
@@ -9,25 +9,25 @@ function FarmersLogHome(props) {
     let tabArray = [
         <div className='tiles'>
             <button className="pressable" type="button" onClick={props.update("Food")}>                            
-                <div className='banner'>
+                <div className='item-name'>
                     <h4>Food</h4>
                 </div> 
                 <img src="../../../../../data/images/potato.png" alt="buttonpng" border="0"/>
             </button>
             <button className="pressable" type="button" onClick={props.update("Ammonium")}>                            
-                <div className='banner'>
+                <div className='item-name'>
                     <h4>Ammonium</h4>
                 </div> 
                 <img src="../../../../../data/images/ammonium.png" alt="buttonpng" border="0"/>
             </button>
             <button className="pressable" type="button" onClick={props.update("Nitrogen")}>
-                <div className='banner'>
+                <div className='item-name'>
                     <h4>Nitrogen</h4>
                 </div> 
                 <img src="../../../../../data/images/potato.png" alt="buttonpng" border="0"/>
             </button>
             <button className="pressable" type="button" onClick={props.update("Nitrogen Runoff")}>
-                <div className='banner'>
+                <div className='item-name'>
                     <h4>Nitrogen Runoff</h4>
                 </div> 
                 <img src="../../../../../data/images/potato.png" alt="buttonpng" border="0"/>
@@ -37,25 +37,25 @@ function FarmersLogHome(props) {
 
         <div className='tiles'>
             <button className="pressable" type="button" onClick={props.update("Potato Plant")}>
-                <div className='banner'>
+                <div className='item-name'>
                     <h4>Potato Plant</h4>
                 </div> 
                 <img src="../../../../../data/images/plant.png" alt="buttonpng" border="0"/>
             </button>
             <button className="pressable" type="button" onClick={props.update("Food Silo")}>
-                <div className='banner'>
+                <div className='item-name'>
                     <h4>Food Silo</h4>
                 </div> 
                 <img src="../../../../../data/images/potato.png" alt="buttonpng" border="0"/>
             </button>
                 <button className="pressable" type="button" onClick={props.update("Ammonium Silo")}>
-                <div className='banner'>
+                <div className='item-name'>
                     <h4>Ammonium Silo</h4>
                 </div> 
             <img src="../../../../../data/images/potato.png" alt="buttonpng" border="0"/>
             </button>  
             <button className="pressable" type="button" onClick={props.update("Nitrogen Fixator")}>
-                <div className='banner'>
+                <div className='item-name'>
                         <h4>Nitrogen Fixator</h4>
                 </div> 
                 <img src="../../../../../data/images/potato.png" alt="buttonpng" border="0"/>
@@ -65,19 +65,19 @@ function FarmersLogHome(props) {
 
         <div className='tiles'>
             <button className="pressable" type="button" onClick={props.update("Nitrogen Cycle")}>                            
-                <div className='banner'>
+                <div className='item-name'>
                     <h4>Nitrogen Cycle</h4>
                 </div> 
                 <img src="../../../../../data/images/potato.png" alt="buttonpng" border="0"/>
             </button>
             <button className="pressable" type="button" onClick={props.update("Nitrogen Fixation")}>                            
-                <div className='banner'>
+                <div className='item-name'>
                     <h4>Nitrogen Fixation</h4>
                 </div> 
                 <img src="../../../../../data/images/potato.png" alt="buttonpng" border="0"/>
             </button>
             <button className="pressable" type="button" onClick={props.update("Denitrification")}>
-                <div className='banner'>
+                <div className='item-name'>
                     <h4>Denitrification</h4>
                 </div> 
                 <img src="../../../../../data/images/potato.png" alt="buttonpng" border="0"/>
@@ -86,19 +86,21 @@ function FarmersLogHome(props) {
 
     return (
             <div className='main'>
-                <ul className='center'>
+                <ul className='main-container'>
                     <ul>
                         <h1>Farmer's Log Table of Contents</h1>
                         <p>Welcome to the Farmer's Log.</p>
                         <p>Here is a list of all the current pages:</p>
                     </ul>
-
-                        <div className="tab">
-                            <button className='tabule' onClick={() => setSelectedIndex(0)}>Resources</button>
-                            <button className='tabule' onClick={() => setSelectedIndex(1)}>Tile</button>
-                            <button className='tabule' onClick={() => setSelectedIndex(2)}>Concepts</button>
+                        <div>
+                            <div className="tab">
+                                <button className='tabule' onClick={() => setSelectedIndex(0)}>Resources</button>
+                                <button className='tabule' onClick={() => setSelectedIndex(1)}>Tile</button>
+                                <button className='tabule' onClick={() => setSelectedIndex(2)}>Concepts</button>
+                            </div>
+                            {tabArray[selectedIndex]}
                         </div>
-                        {tabArray[selectedIndex]}
+
                 </ul>
             </div> 
     );
